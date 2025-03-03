72f9174a7d4722797d235d653e71febe00b1ba71
lyhzh <liskyair@hotmail.com>
2025-03-03 12:26:12 +0800

    最新公告对接后台接口

--- pingGaoWebDevelop/src/views/_2_informationCenter/latestAnnouncement/index-1.vue
+++ pingGaoWebDevelop/src/views/_2_informationCenter/latestAnnouncement/index-1.vue
@@ -1,13 +1,15 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import Item2 from "@/components/Item-2.vue";
+import MyContent from "@/components/MyContent.vue";
+import MyPagination from "@/components/MyPagination.vue";
+import MyTitle from "@/components/MyTitle.vue";
+import router from "@/router/index.js";
+import httpUtils from "@/utils/httpUtils.js";
+import { onMounted, ref } from "vue";
 
 defineOptions({
   name: "NewsCenterIndex5-1",
 });
-import MyTitle from "@/components/MyTitle.vue";
-import MyContent from "@/components/MyContent.vue";
-import Item2 from "@/components/Item-2.vue";
-import MyPagination from "@/components/MyPagination.vue";
 
 const contentBox = ref(null);
 onMounted(() => {
@@ -21,7 +23,6 @@ onMounted(() => {
       const windowHeight = window.innerHeight;
 
       // 判断元素是否进入可视区域
-
       if (contentTop < windowHeight) {
         contentBox.value.classList.add("show");
       } else {
@@ -30,6 +31,86 @@ onMounted(() => {
     });
   }
 });
+
+document.title = "最新公告";
+const categoryId = 20;
+const mainNews = ref([]);
+const topNews = ref({});
+const pageNo = ref(1);
+const pageSize = ref(10);
+const pageMax = ref(1);
+const hasMore = ref(true);
+
+// 格式化时间戳为 YYYY-MM-DD 格式
+function formatTimestamp (timestamp) {
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}.${month}.${day}`;
+}
+
+// 格式化时间戳为 YYYY-MM-DD 格式
+function formatTimestampObj (timestamp) {
+  const date = new Date(timestamp);
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return {
+    year: `${year}`,
+    month: `${month}.${day}`,
+  };
+}
+
+async function getData () {
+  const queryString = new URLSearchParams({
+    pageNo: pageNo.value,
+    pageSize: pageSize.value,
+  }).toString();
+
+  console.log("获取数据", queryString);
+  const response = await httpUtils.get(`/cms/category/${categoryId}/news?${queryString.toString()}`);
+  const { data } = await response.json();
+  const page = data.page;
+  page.list.forEach((i) => {
+    i.publishTime = formatTimestamp(i.publishTime);
+    i.timeObj = formatTimestampObj(i.publishTime);
+  });
+
+  console.log(page.list);
+  if (page.value === 1) {
+    topNews.value = page.list[0];
+    mainNews.value = page.list.slice(1);
+  } else {
+    topNews.value = page.list[0];
+    mainNews.value = page.list;
+  }
+  pageMax.value = Math.ceil(page.total / pageSize.value);
+}
+
+function pageChange (pageNumber) {
+  pageNo.value = pageNumber;
+  getData();
+}
+
+function toDetail (id) {
+  console.log(id);
+
+  if (id) {
+    router.push({
+      name: "newsDetail",
+      params: {
+        id,
+      },
+    });
+
+    setTimeout(() => {
+      window.location.reload();
+    }, 10);
+  }
+}
+
+getData();
 </script>
 
 <template>
@@ -42,11 +123,12 @@ onMounted(() => {
         <div ref="contentBox" style="margin-top: 3.6rem">
           <MyContent
             class="content"
-            title="关于开设拖欠中小企业账款诉求受理渠道的公示"
-            content1="为认真落实国务院国资委关于助力中小企业纾困解难促进协同发展的工作要求，根据《保障中小企业款项支付条例》有关规定，完善应付账款管理
-机制，平高集团有限公司（以下简称“集团”）开设清理拖欠中小企业款项诉求反映受理渠道。请各中小企业供应商、服务商对集团所属子企业拖
-欠无争议应付账款行为进行监督。"
-            :buttonFlag="true"
+            :detail-id="topNews.id"
+            :title="topNews.title"
+            :content1="topNews.description"
+            :button-flag="true"
+            content2=""
+            @click-item="toDetail"
           />
         </div>
         <div class="bodyBg1"></div>
@@ -54,9 +136,16 @@ onMounted(() => {
       <div class="body">
         <div class="items">
           <ul>
-            <li v-for="i in 5" :key="i">
-              <router-link to="/informationCenter/latestAnnouncement-2">
-                <Item2 bgColor="#def1fb" size="big" />
+            <li v-for="(item, index) in mainNews" :key="index">
+              <router-link
+                :to="{
+                  name: 'newsDetail',
+                  params: {
+                    id: item.id
+                  }
+                }"
+              >
+                <Item2 :time="item.publishTime" :text="item.title" bg-color="#def1fb" size="big" />
               </router-link>
             </li>
           </ul>
@@ -64,7 +153,13 @@ onMounted(() => {
         <div
           style="display: flex; justify-content: center; margin-top: 3.5rem; margin-bottom: 5.3rem"
         >
-          <MyPagination />
+          <MyPagination
+            v-if="hasMore" :total="pageMax" :current="pageNo" class="pagination"
+            @page-change="pageChange"
+          />
+          <p v-else style="font-size: 24px;">
+            暂无更多
+          </p>
         </div>
       </div>
     </div>
