facbed1a37c931f0700b7d7270b845af3ce2a8fe
lyhzh <liskyair@hotmail.com>
2025-03-10 22:41:19 +0800

    关于平高图片对接后台接口

--- pingGaoWebDevelop/src/views/_1_aboutPinggao/approachPinggao/myIndex.vue
+++ pingGaoWebDevelop/src/views/_1_aboutPinggao/approachPinggao/myIndex.vue
@@ -1,88 +1,69 @@
 <script setup>
-import MyTitle from '@/components/MyTitle.vue'
-import MyButton from '@/components/MyButton.vue'
-import { ref, onMounted } from 'vue'
+import MyTitle from "@/components/MyTitle.vue";
+import { ref, nextTick } from "vue";
+import httpUtils from "@/utils/httpUtils.js";
 
-const imgRef = ref(null)
-const isVisible = ref(null)
+const imgRef = ref(null);
+const isVisible = ref(null);
 // 创建交叉观察器
 const createObserver = (refElement, isVisible) => {
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
-        const { intersectionRatio } = entry
+        const { intersectionRatio } = entry;
         // 设置触发条件：元素进入视口 50% 以上时触发
         if (intersectionRatio >= 0) {
-          isVisible.value = true
-          observer.disconnect() // 元素可见后停止观察
+          isVisible.value = true;
+          observer.disconnect(); // 元素可见后停止观察
         }
-      })
+      });
     },
     {
       root: null, // 使用浏览器视口作为根元素
-      rootMargin: '0px', // 无额外的边距
+      rootMargin: "0px", // 无额外的边距
       threshold: 0, // 当元素的 50% 进入视口时触发
-    }
-  )
+    },
+  );
 
   if (refElement.value) {
-    observer.observe(refElement.value)
+    refElement.value.forEach((el) => {
+      observer.observe(el);
+    });
   }
-}
+};
 // 初始化所有的观察器
 const initializeObservers = () => {
-  createObserver(imgRef, isVisible)
+  createObserver(imgRef, isVisible);
+};
+
+const imageList = ref([]);
+
+async function getData () {
+  const response = await httpUtils.get("/cms/banner/about/list");
+  const { data } = await response.json();
+  console.log(data);
+
+  imageList.value = data;
+
+  await nextTick();
+
+  initializeObservers();
 }
-onMounted(initializeObservers) // 在组件挂载时调用
+
+getData();
 </script>
 
 <template>
   <div class="approachPinggao">
     <MyTitle title="走进平高" english="ENTERING PINGGAO"></MyTitle>
     <div class="pics">
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic1.png" alt="Image 1"
-           class="grid-item1 grid_item " ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic2.png" alt="Image 2"
-           class="grid-item2 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic3.png" alt="Image 3"
-           class="grid-item3 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic4.png" alt="Image 4"
-           class="grid-item4 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic5.png" alt="Image 5"
-           class="grid-item5 grid_item " ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic6.png" alt="Image 6"
-           class="grid-item6 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic7.png" alt="Image 7"
-           class="grid-item7 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic3.png" alt="Image 8"
-           class="grid-item8 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic8.png" alt="Image 9"
-           class="grid-item9 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic9.png" alt="Image 10"
-           class="grid-item10 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic10.png" alt="Image 11"
-           class="grid-item11 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic11.png" alt="Image 12"
-           class="grid-item12 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
-      <img src="@/assets/imgs/_1_aboutPinggaoImgs/pic3.png" alt="Image 13"
-           class="grid-item13 grid_item" ref="imgRef"
-           :class="{ 'scale-up': isVisible}" />
+      <img v-for="(image,index) in imageList" :key="image.id" :src="image.images" :alt="image.name"
+           class="grid_item" ref="imgRef"
+           :class="{ 'scale-up': isVisible, ['grid-item'+(index+1)]:true}" />
     </div>
-<!--    <div class="footer">-->
-<!--      <MyButton class="myButton" text="加载更多"></MyButton>-->
-<!--    </div>-->
+    <!--    <div class="footer">-->
+    <!--      <MyButton class="myButton" text="加载更多"></MyButton>-->
+    <!--    </div>-->
   </div>
 </template>
 
@@ -101,7 +82,6 @@ onMounted(initializeObservers) // 在组件挂载时调用
   margin-top: 3.75rem;
   display: grid;
   grid-template-columns: repeat(3, 1fr); /* 3列 */
-  grid-template-rows: repeat(6, 1fr); /* 6行 */
   gap: 0.625rem;
   grid-template-areas:
     "a b c"
