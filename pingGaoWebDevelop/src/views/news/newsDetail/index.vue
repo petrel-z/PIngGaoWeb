f3e3969985221cef69ab5c1556fb886ad19f41d3
Fly <1551681944@qq.com>
2025-03-21 20:20:49 +0800

    新闻详情页英文版

--- pingGaoWebDevelop/src/views/news/newsDetail/index.vue
+++ pingGaoWebDevelop/src/views/news/newsDetail/index.vue
@@ -1,5 +1,19 @@
-<template></template>
+<template>
+  <div class="container">
+    <div class="container-title">
+      China Electrical Equipment held the second session of the first employee representative
+      conference
+    </div>
+    <div class="container-body"></div>
+  </div>
+</template>
 
-<script setup></script>
+<script setup>
+import { defineComponent } from "vue";
+
+defineComponent({
+  name: "NewsDetail",
+});
+</script>
 
 <style></style>
